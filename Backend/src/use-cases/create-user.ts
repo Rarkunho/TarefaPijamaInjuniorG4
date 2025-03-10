2bd6470e03d4fd765fa22c4d6694011a67fe5349
Rarkunho <112668835+Rarkunho@users.noreply.github.com>
2025-03-10 14:38:40 -0300

    chore: use case de criar usuario

---
+++ Backend/src/use-cases/create-user.ts
@@ -0,0 +1,29 @@
+import { hash } from "bcryptjs";
+import { UsersRepository } from "src/repositories/users-repository";
+import { UserAlreadyExists } from "./errors/user-exists";
+
+interface CreateUserCaseRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export class CreateUserCase {
+    constructor(private usersRepository: UsersRepository) {}
+
+    async execute({ name, email, password }: CreateUserCaseRequest) {
+        const userWithSameEmail = await this.usersRepository.findByEmail(email);
+
+        if (userWithSameEmail) {
+            throw new UserAlreadyExists();
+        }
+
+        const passwordHash = await hash(password, 6);
+
+        await this.usersRepository.create({
+            name,
+            email,
+            password: passwordHash,
+        });
+    }
+}
