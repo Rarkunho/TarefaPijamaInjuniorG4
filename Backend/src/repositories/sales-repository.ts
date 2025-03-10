052e8bc3298a6cca80ddde211719f6d2d07cd2e4
Allber Ferreira <allber31415@gmail.com>
2025-03-09 22:02:03 -0300

    feat: Criação da interface do repositório de vendas

--- Backend/src/repositories/sales-repository.ts
+++ Backend/src/repositories/sales-repository.ts
@@ -1,5 +1,25 @@
-import { Prisma, Sale } from "@prisma/client";
+import { PaymentMethod, Prisma, Sale } from "@prisma/client";
+
+interface SaleInfoResponse
+extends Prisma.SaleUncheckedCreateInput,
+        Prisma.AddressUncheckedCreateInput {
+    quantity: number;
+    price: number;
+}
+
+interface SaleUpdateInput {
+    buyerName?: string;
+    cpf?: string;
+    price?: number;
+    paymentMethod?: PaymentMethod;
+    installments?: number;
+    cardNumber?: string;
+}
 
 interface SalesRepository {
     create(saleData: Prisma.SaleUncheckedCreateInput): Promise<Sale>;
+    update(saleId: string, updateData: SaleUpdateInput): Promise<Sale | null>;
+    delete(saleId: string): Promise<Sale | null>;
+    findById(saleId: string): Promise<Sale | null>;
+    getSaleInfo(saleId: string): Promise<SaleInfoResponse | null>;
 }
