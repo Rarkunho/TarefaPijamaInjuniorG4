c3fff2669c1b918c7c381969e8f217cd25b7748e
Rarkunho <112668835+Rarkunho@users.noreply.github.com>
2025-03-10 14:44:00 -0300

    feat: rota create user

---
+++ Backend/src/http/controllers/users/create.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { CreateUserCase } from "src/use-cases/create-user";
+import { PrismaUsersRepository } from "src/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExists } from "src/use-cases/errors/user-exists";
+
+export async function createUser(request : FastifyRequest, reply : FastifyReply){
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    })
+
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    try {
+        const prismaUsersRepository = new PrismaUsersRepository()
+        const createUserCase = new CreateUserCase(prismaUsersRepository)
+        await createUserCase.execute({
+            name,
+            email,
+            password
+        })
+    } catch (error) {
+        if (error instanceof (UserAlreadyExists)) {
+            return reply.status(409).send({ message: error.message })
+        }
+        throw error
+    }
+
+    return reply.status(201).send('Usuario Registrado com sucesso')
+}
