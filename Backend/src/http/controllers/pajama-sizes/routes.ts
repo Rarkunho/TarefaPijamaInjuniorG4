02b7d031d7bae80a95327782ddab0c1ed4d061cd
Allber Ferreira <alferreira@id.uff.br>
2025-03-13 20:24:33 -0300

    refactor: Reorganização da disposição das rotas

--- Backend/src/http/controllers/pajama-sizes/routes.ts
+++ Backend/src/http/controllers/pajama-sizes/routes.ts
@@ -4,7 +4,7 @@ import { getAllPajamaSizeByPajamaId } from "./get-all-pajama-size-by-pajama-id";
 import { getPajamaSizeByPajamaId } from "./get-pajama-size-by-pajama-id";
 
 export async function pajamaStockRoutes(app: FastifyInstance) {
-    app.get('/pajamas/stock/:pajamaId', getAllPajamaSizeByPajamaId);
     app.get('/pajamas/stock/:pajamaId/:size', getPajamaSizeByPajamaId);
+    app.get('/pajamas/stock/:pajamaId', getAllPajamaSizeByPajamaId);
     app.patch('/pajamas/stock/:pajamaId/:size', updateStockQuantity);
 }
