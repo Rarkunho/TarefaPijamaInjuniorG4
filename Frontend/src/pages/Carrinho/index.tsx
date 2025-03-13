30f0b45ba6abf2e09177d10557974cc380e5558d
Bruno Coitinho <bruno.pereira1988@gmail.com>
2025-03-13 18:15:24 -0300

    feat: criado item do carrinho

--- Frontend/src/pages/Carrinho/index.tsx
+++ Frontend/src/pages/Carrinho/index.tsx
@@ -2,10 +2,15 @@ import styles from "./styles.module.css";
 import logoCarrinho from "../../assets/ComprasActive.png";
 import logoFavoritos from "../../assets/FavoritoInactive.png";
 import { useNavigate } from "react-router-dom";
+import fotoTeste from "../../assets/pijamaTeste.png"
+import SizeButton from "../../components/Buttons/SizeButton"
+
+import QuantityButton from "../../components/Buttons/QuantityButton"
+
 
 
 export default function Carrinho() {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
     
     return (
         <>
@@ -27,8 +32,62 @@ export default function Carrinho() {
                 />
                 <h2 className={`${styles.title} ${styles.inactive}`}  onClick={() => navigate("/favoritos")}>Favoritos</h2>
             </nav>
-            <main className={`${styles.fundo}`}>
-                <h3>a</h3>
+            <main className={`${styles.fundo} ${styles.main}`}>
+                <div className={styles.cart__container}>
+                    
+                    <div className={styles.cart__item}>
+                        <div style={{display: "flex", gap: "2.7rem"}}>
+                            <img className={styles.cart__item__img} src={fotoTeste} alt="Foto do Pijama"/>
+                            <div className={styles.cart__item__infosize}>
+                                <div className={styles.cart__item__title}>
+                                    <h2>PIJAMA FEMININO LONGO - ESTAMPA POÁ</h2>
+                                    <p className={styles.cart__item__ref}>Ref: #123456</p>
+                                </div>
+                                <SizeButton
+                                text='M'
+                                isActive={true} onClick={function (): void {} }
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.cart__item__qtdprice}>
+                            <div></div>
+                            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end', gap:'2.7rem'}}>
+                                <div className={styles.quantidade}>
+                                    <h2 className={styles.tituloItalico}>Quantidade: </h2>
+                                    <QuantityButton/>
+                                </div>
+                                <h2 className={styles.cart__item__price}>R$ 78,90</h2>
+                            </div>
+                            <p className={styles.cart__item__msg}>Ainda temos <span>x</span> peças do tamanho escolhido <br/>em nosso estoque!</p>
+                        </div>
+                    </div>
+                    <div className={styles.cart__item}>
+                        <div style={{display: "flex", gap: "2.7rem"}}>
+                            <img className={styles.cart__item__img} src={fotoTeste} alt="Foto do Pijama"/>
+                            <div className={styles.cart__item__infosize}>
+                                <div className={styles.cart__item__title}>
+                                    <h2>PIJAMA FEMININO LONGO - ESTAMPA POÁ</h2>
+                                    <p className={styles.cart__item__ref}>Ref: #123456</p>
+                                </div>
+                                <SizeButton
+                                text='M'
+                                isActive={true} onClick={function (): void {} }
+                                />
+                            </div>
+                        </div>
+                        <div className={styles.cart__item__qtdprice}>
+                            <div></div>
+                            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end', gap:'2.7rem'}}>
+                                <div className={styles.quantidade}>
+                                    <h2 className={styles.tituloItalico}>Quantidade: </h2>
+                                    <QuantityButton/>
+                                </div>
+                                <h2 className={styles.cart__item__price}>R$ 78,90</h2>
+                            </div>
+                            <p className={styles.cart__item__msg}>Ainda temos <span>x</span> peças do tamanho escolhido <br/>em nosso estoque!</p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     );
